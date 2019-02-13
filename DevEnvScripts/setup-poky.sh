d81f53681bfadf9c299d47ec24e381de273cc496
Phil <PhilboBaggins@users.noreply.github.com>
2019-02-14 03:27:11 +1100

    Initial commit

---
+++ DevEnvScripts/setup-poky.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# Download and configure Poky
+
+# https://wiki.yoctoproject.org/wiki/Releases
+# https://wiki.yoctoproject.org/wiki/Stable_branch_maintenance
+POKY_BRANCH="thud"
+
+SOURCE_DIR="$HOME/sources"
+BUILD_DIR="$HOME/build-poky"
+
+function cloneRepoBranch()
+{
+    local BRANCH="$1"
+    local URL="$2"
+    local DEST_DIR="${SOURCE_DIR}/$(basename -- "$URL")"
+    mkdir -p "${SOURCE_DIR}"
+    if [ -d "$DEST_DIR" ]; then
+        pushd "$DEST_DIR"
+        git pull
+        popd
+    else
+        git clone -b "$BRANCH" "$URL" "$DEST_DIR"
+    fi
+}
+
+cloneRepoBranch "$POKY_BRANCH" "git://git.yoctoproject.org/poky"
+cloneRepoBranch "$POKY_BRANCH" "git://git.openembedded.org/meta-openembedded"
+
+rm -rf "${BUILD_DIR}"
+source "${SOURCE_DIR}/poky/oe-init-build-env" "${BUILD_DIR}"
+
+# Set up bblayers.conf
+cat > "${BUILD_DIR}/conf/bblayers.conf" << EOF
+# LAYER_CONF_VERSION is increased each time build/conf/bblayers.conf
+# changes incompatibly
+POKY_BBLAYERS_CONF_VERSION = "2"
+
+BBPATH = "\${TOPDIR}"
+BBFILES ?= ""
+BBLAYERS ?= " \\
+    ${SOURCE_DIR}/poky/meta \\
+    ${SOURCE_DIR}/poky/meta-poky \\
+    ${SOURCE_DIR}/poky/meta-yocto-bsp \\
+    ${SOURCE_DIR}/meta-openembedded/meta-oe \\
+    ${SOURCE_DIR}/meta-openembedded/meta-multimedia \\
+    "
+BBLAYERS_NON_REMOVABLE ?= " \\
+    ${SOURCE_DIR}/poky/meta \\
+    ${SOURCE_DIR}/poky/meta-poky \\
+    "
+EOF
+
+# Set up local.conf
+cat >> "${BUILD_DIR}/conf/local.conf" << 'EOF'
+
+#
+# My config
+#
+INHERIT += "rm_work"
+MACHINE ?= "qemux86-64"
+EOF
---
+++ Vagrantfile
@@ -0,0 +1,21 @@
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+    #config.vm.box = "debian/stretch64"     # Debian 9
+    #config.vm.box = "hashicorp/precise64"  # Ubuntu 12.04 LTS
+    config.vm.box = "ubuntu/trusty64"      # Ubuntu 14.04 LTS
+    #config.vm.box = "ubuntu/xenial64"      # Ubuntu 16.04 LTS
+
+    config.disksize.size = '70GB'  # https://github.com/sprotheroe/vagrant-disksize
+
+    config.vm.provision :shell, privileged: true,  path: "DevEnvScripts/install-os-packages.sh"
+    config.vm.provision :shell, privileged: false, path: "DevEnvScripts/setup-poky.sh"
+
+    config.vm.provider :virtualbox do |v|
+        v.cpus = 2
+        v.memory = 3072
+    end
+end
